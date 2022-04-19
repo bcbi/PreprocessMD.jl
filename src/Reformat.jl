29e5f128a51b0c998aa712f2c9b3a80b8afb8fa0
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-19 15:48:05 -0400

    Add long_to_wide()

--- src/Reformat.jl
+++ src/Reformat.jl
@@ -4,4 +4,12 @@ import DataFrames
 
 greet() = print("Hello World!")
 
+function long_to_wide(df::AbstractDataFrame, x::Symbol, y::Symbol)::AbstractDataFrame
+        B = unstack(combine(groupby(df, [x,y]), nrow => :count), x, y, :count, fill=0)
+        for q in names(select(B, Not(x)))
+                B[!,q] = B[!,q] .!= 0
+        end
+        return B
+end
+
 end # module
