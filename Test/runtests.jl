93e5db907e2d8da939bacac84065b0156cb21344
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-19 15:53:17 -0400

    Test successful

--- Test/runtests.jl
+++ Test/runtests.jl
@@ -14,6 +14,7 @@ B = long_to_wide(A, :a, :b)
 C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
 @test B == C
 
+
 #=
 A = DataFrame(x = [0,1,5,1,2,5,4,3,8,6,9,9,5,1,1,3], 
 	y = ['a','b','c','a','a','a','c','d', 'a','b','c','a','a','a','c','d'],
