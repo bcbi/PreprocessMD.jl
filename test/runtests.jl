f82f3cb5ff9ac9d454dc641fc344a3df9be4e625
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-20 15:15:16 -0400

    Function-level test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,46 +5,40 @@ using Test
 using PreprocessMD
 using DataFrames
 
-@testset verbose = true "Intended exceptions" begin
-	@testset "MethodError" begin
-		for x in [12, 1.0, "", x -> x]
-			@test_throws MethodError long_to_wide(x)
+
+@testset verbose = true "long_to_wide()" begin
+
+	@testset verbose = true "Intended exceptions" begin
+		@testset "MethodError" begin
+			for x in [12, 1.0, "", x -> x]
+				@test_throws MethodError long_to_wide(x)
+			end
+		end
+		@testset "DomainError" begin
+			for x in [DataFrame(), DataFrame(x = [0,1,2,3])]
+				@test_throws DomainError long_to_wide(x)
+			end
 		end
 	end
-	@testset "DomainError" begin
-		for x in [DataFrame(), DataFrame(x = [0,1,2,3])]
-			@test_throws DomainError long_to_wide(x)
-		end
+	@testset verbose = true "Simple examples" begin
+		A = DataFrame(a=[1,2], b=['x','y'])
+		@test A == DataFrame(a=[1,2], b=['x','y'])
+
+		A = DataFrame(a=[1,2,1], b=['x','y','y'])
+		B = long_to_wide(A, :a, :b)
+		C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
+		@test B == C
+
+		B = long_to_wide(A, :a)
+		@test B == C
+
+		B = long_to_wide(A)
+		@test B == C
+
+		B = long_to_wide(A, "a", "b")
+		@test B == C
 	end
-end;
-
-A = DataFrame(a=[1,2], b=['x','y'])
-@test A == DataFrame(a=[1,2], b=['x','y'])
-
-A = DataFrame(a=[1,2,1], b=['x','y','y'])
-B = long_to_wide(A, :a, :b)
-C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
-@test B == C
-
-B = long_to_wide(A, :a)
-@test B == C
-
-B = long_to_wide(A)
-@test B == C
-
-B = long_to_wide(A, "a", "b")
-@test B == C
-
-x = DataFrame()
-
-
-
-
-
-
-
-
-
+end
 
 #=
 A = DataFrame(x = [0,1,5,1,2,5,4,3,8,6,9,9,5,1,1,3], 
