8885230239ab0186ce488fa374078c61c8e69e1f
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-20 12:05:29 -0400

    Add file again

---
+++ src/ReformatMD.jl
@@ -0,0 +1,98 @@
+module ReformatMD
+
+export long_to_wide, wide_to_long
+
+using DataFrames
+
+greet() = print("Hello World!")
+
+function long_to_wide(df::AbstractDataFrame, x=nothing, y=nothing)::AbstractDataFrame
+
+	if size(df)[2] < 2
+		#@warn "ReformatMD: DataFrame must have at lease 2 columns"
+		throw(DomainError())
+		return DataFrame()
+	end
+
+	if isnothing(x)
+		x = Symbol(names(df)[1])
+	end
+	if isnothing(y)
+		#y = Symbol.(names(select(df, Not(x))))
+		y = Symbol(names(df)[2])
+	end
+
+        B = unstack(combine(groupby(df, [x,y]), nrow => :count), x, y, :count, fill=0)
+        for q in names(select(B, Not(x)))
+                B[!,q] = B[!,q] .!= 0
+        end
+        return B
+end
+
+wide_to_long = stack
+
+end # module ReformatMD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
