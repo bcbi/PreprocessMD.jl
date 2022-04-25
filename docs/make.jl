c21d883768e7d9a09a9a0cc049fba1897314ebc4
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-25 14:20:21 -0400

    Remove optional arguments

--- docs/make.jl
+++ docs/make.jl
@@ -4,17 +4,7 @@ using Documenter, PreprocessMD
 
 makedocs(;
 	authors="Ashlin Harris",
-	sitename="PreprocessMD.jl",
-	# root = PreprocessMD.package_directory("docs",),
-	format=Documenter.HTML(;
-		prettyurls=get(ENV, "CI", "false") == "true",
-		canonical="https://ashlinharris.github.io/AddLatest.jl",
-		assets=String[],
-	),
-	pages=[
-		"Home" => "index.md",
-	],
-	strict=true,
+	sitename="PreprocessMD",
 )
 
 deploydocs(
