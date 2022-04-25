c28ca6b78f5f932870b00be63afde85390a2be2a
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-25 13:01:21 -0400

    Add options

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,16 @@ using Documenter, PreprocessMD
 
 makedocs(
 	sitename="PreprocessMD",
+	root = PreprocessMD.package_directory("docs",),
+        pages = ["Home" => "index.md",]
 )
 
 deploydocs(
 	repo = "github.com/ashlinharris/PreprocessMD.jl.git",
 	versions = nothing,
+	target = "build",
+	branch = "gh-pages",
+	devbranch = "master",
+	devurl = "development",
 )
-
-
 
