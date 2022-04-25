00dfea3769b42c142d60be939d59d44a8ee29f85
Ashlin Harris <ashlin_harris@brown.edu>
2022-04-25 14:01:05 -0400

    Add options

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,19 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, PreprocessMD
 
-makedocs(
-	sitename="PreprocessMD",
+makedocs(;
+	authors="Ashlin Harris",
+	sitename="PreprocessMD.jl",
 	# root = PreprocessMD.package_directory("docs",),
+	format=Documenter.HTML(;
+		prettyurls=get(ENV, "CI", "false") == "true",
+		canonical="https://ashlinharris.github.io/AddLatest.jl",
+		assets=String[],
+	),
+	pages=[
+		"Home" => "index.md",
+	],
+	strict=true,
 )
 
 deploydocs(
