7a90a66610ca91b08e32f8d4e18e48762bb27127
Ashlin Harris <ashlin_harris@brown.edu>
2022-05-05 14:29:52 -0400

    Add UndefVar sanity check

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ using Test
 
 	@testset verbose = true "Sanity check" begin
 		@test true
+		@test_throws UndefVarError NONEXISTENT_FUNCTION()
 	end
 
 	@testset verbose = true "add_label_column!()" begin
@@ -80,8 +81,8 @@ using Test
 			end
 			@testset "MethodError" begin
 				for x in [12, 1.0, "", x -> x]
-					@test_throws MethodError append_target_column(x, DataFrame())
-					@test_throws MethodError append_target_column(DataFrame(), x)
+					@test_throws MethodError append_target_column!(x, DataFrame())
+					@test_throws MethodError append_target_column!(DataFrame(), x)
 				end
 			end
 		end
