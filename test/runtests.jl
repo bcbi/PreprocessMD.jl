bc255d096d9dd842583ee67acd3ebaae1a14af4b
Ashlin Harris <ashlin_harris@brown.edu>
2022-05-05 12:24:58 -0400

    Use deepcopy()

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,10 @@ using Test
 end
 
 @testset verbose = true "add_label_column!()" begin
+
+#testsets: bad options, default options
+
+
 	long = DataFrame(
 	       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
 	       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
@@ -22,7 +26,6 @@ end
 		name=["bbb","ccc","fff"],
 		r=["BBB","CCC","FFF"],
 	)
-
 
 	results = DataFrame(
 		name=["aaa","bbb","ccc","eee"],
@@ -35,19 +38,19 @@ end
 		LABEL=[false,  true,  true, false,],
 	)
 
-	new = short
+	new = deepcopy(short)
 	add_label_column!(new, X, :name, :name, :LABEL)
 	@test new == results
 
-	new = short
+	new = deepcopy(short)
 	add_label_column!(new, X, :name, :name)
 	@test new == results
 
-	new = short
+	new = deepcopy(short)
 	add_label_column!(new, X, :name)
 	@test new == results
 
-	new = short
+	new = deepcopy(short)
 	add_label_column!(new, X)
 	@test new == results
 
