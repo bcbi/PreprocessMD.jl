3f8d8bc7ca18df033c9af8f2fece07412b5ba2af
Ashlin Harris <ashlin_harris@brown.edu>
2022-05-05 14:21:07 -0400

    Format

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,40 +7,81 @@ using Test
 
 @testset verbose = true "PreprocessMD" begin
 
-@testset verbose = true "Sanity check" begin
-	@test true
-end
+	@testset verbose = true "Sanity check" begin
+		@test true
+	end
 
-@testset verbose = true "add_label_column!()" begin
-	@testset verbose = true "Intended exceptions" begin
-		@testset "DomainError" begin
-				@test_throws DomainError add_label_column!(
-					DataFrame(),
-					DataFrame()
+	@testset verbose = true "add_label_column!()" begin
+		@testset verbose = true "Intended exceptions" begin
+			@testset "DomainError" begin
+					@test_throws DomainError add_label_column!(
+						DataFrame(),
+						DataFrame()
+					)
+					@test_throws DomainError add_label_column!(
+						DataFrame(),
+						DataFrame(x = []),
+					)
+					@test_throws DomainError add_label_column!(
+						DataFrame(x = []),
+						DataFrame(),
+					)
+					@test_throws DomainError add_label_column!(
+						DataFrame(x = []),
+						DataFrame(x = []),
+					)
+					@test_throws DomainError add_label_column!(
+						DataFrame(x = [1,2]),
+						DataFrame(x = []),
+					)
+					@test_throws DomainError add_label_column!(
+						DataFrame(x = []),
+						DataFrame(x = [1,2]),
+					)
+				end
+			end
+			@testset verbose = true "ArgumentError" begin
+
+				# DataFrame definitions
+				long = DataFrame(
+				       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
+				       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
+				       )
+				short = pivot(long)
+				X = DataFrame(
+					name=["bbb","ccc","fff"],
+					r=["BBB","CCC","FFF"],
 				)
-				@test_throws DomainError add_label_column!(
-					DataFrame(),
-					DataFrame(x = []),
+				results = DataFrame(
+					name=["aaa","bbb","ccc","eee"],
+					x=[ true, false, false, false,],
+					w=[ true,  true, false,  true,],
+					y=[ true, false,  true, false,],
+					z=[false, false,  true, false,],
+					q=[ true, false, false, false,],
+					a=[false, false,  true, false,],
+					LABEL=[false,  true,  true, false,],
 				)
-				@test_throws DomainError add_label_column!(
-					DataFrame(x = []),
-					DataFrame(),
+
+				new = deepcopy(short)
+				@test_throws ArgumentError add_label_column!(
+					new, X, :name, :name, :w
 				)
-				@test_throws DomainError add_label_column!(
-					DataFrame(x = []),
-					DataFrame(x = []),
+
+				new = deepcopy(short)
+				@test_throws ArgumentError add_label_column!(
+					new, X, :name, :NONEXISTENT
 				)
-				@test_throws DomainError add_label_column!(
-					DataFrame(x = [1,2]),
-					DataFrame(x = []),
+
+				new = deepcopy(short)
+				@test_throws ArgumentError add_label_column!(
+					new, X, :NONEXISTENT
 				)
-				@test_throws DomainError add_label_column!(
-					DataFrame(x = []),
-					DataFrame(x = [1,2]),
-				)
+
 			end
 		end
-		@testset verbose = true "ArgumentError" begin
+
+		@testset verbose = true "Default options" begin
 
 			# DataFrame definitions
 			long = DataFrame(
@@ -62,190 +103,148 @@ end
 				a=[false, false,  true, false,],
 				LABEL=[false,  true,  true, false,],
 			)
+			new = deepcopy(short)
+
+			add_label_column!(new, X, :name, :name)
+			@test new == results
 
 			new = deepcopy(short)
-			@test_throws ArgumentError add_label_column!(
-				new, X, :name, :name, :w
-			)
+			add_label_column!(new, X, :name)
+			@test new == results
 
 			new = deepcopy(short)
-			@test_throws ArgumentError add_label_column!(
-				new, X, :name, :NONEXISTENT
+			add_label_column!(new, X)
+			@test new == results
+		end
+
+
+		@testset verbose = true "Simple examples" begin
+
+			# DataFrame definitions
+			long = DataFrame(
+			       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
+			       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
+			       )
+			short = pivot(long)
+			X = DataFrame(
+				name=["bbb","ccc","fff"],
+				r=["BBB","CCC","FFF"],
 			)
+			results = DataFrame(
+				name=["aaa","bbb","ccc","eee"],
+				x=[ true, false, false, false,],
+				w=[ true,  true, false,  true,],
+				y=[ true, false,  true, false,],
+				z=[false, false,  true, false,],
+				q=[ true, false, false, false,],
+				a=[false, false,  true, false,],
+				LABEL=[false,  true,  true, false,],
+			)
+			new = deepcopy(short)
 
 			new = deepcopy(short)
-			@test_throws ArgumentError add_label_column!(
-				new, X, :NONEXISTENT
-			)
-
+			add_label_column!(new, X, :name, :name, :LABEL)
+			@test new == results
 		end
 	end
 
-	@testset verbose = true "Default options" begin
+	@testset verbose = true "pivot()" begin
 
-		# DataFrame definitions
-		long = DataFrame(
-		       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
-		       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
-		       )
-		short = pivot(long)
-		X = DataFrame(
-			name=["bbb","ccc","fff"],
-			r=["BBB","CCC","FFF"],
-		)
-		results = DataFrame(
-			name=["aaa","bbb","ccc","eee"],
-			x=[ true, false, false, false,],
-			w=[ true,  true, false,  true,],
-			y=[ true, false,  true, false,],
-			z=[false, false,  true, false,],
-			q=[ true, false, false, false,],
-			a=[false, false,  true, false,],
-			LABEL=[false,  true,  true, false,],
-		)
-		new = deepcopy(short)
+		@testset verbose = true "Intended exceptions" begin
+			@testset "MethodError" begin
+				for x in [12, 1.0, "", x -> x]
+					@test_throws MethodError pivot(x)
+				end
+			end
 
-		add_label_column!(new, X, :name, :name)
-		@test new == results
-
-		new = deepcopy(short)
-		add_label_column!(new, X, :name)
-		@test new == results
-
-		new = deepcopy(short)
-		add_label_column!(new, X)
-		@test new == results
-	end
-
-
-	@testset verbose = true "Simple examples" begin
-
-		# DataFrame definitions
-		long = DataFrame(
-		       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
-		       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
-		       )
-		short = pivot(long)
-		X = DataFrame(
-			name=["bbb","ccc","fff"],
-			r=["BBB","CCC","FFF"],
-		)
-		results = DataFrame(
-			name=["aaa","bbb","ccc","eee"],
-			x=[ true, false, false, false,],
-			w=[ true,  true, false,  true,],
-			y=[ true, false,  true, false,],
-			z=[false, false,  true, false,],
-			q=[ true, false, false, false,],
-			a=[false, false,  true, false,],
-			LABEL=[false,  true,  true, false,],
-		)
-		new = deepcopy(short)
-
-		new = deepcopy(short)
-		add_label_column!(new, X, :name, :name, :LABEL)
-		@test new == results
-	end
-
-end
-
-@testset verbose = true "pivot()" begin
-
-	@testset verbose = true "Intended exceptions" begin
-		@testset "MethodError" begin
-			for x in [12, 1.0, "", x -> x]
-				@test_throws MethodError pivot(x)
+			@testset "DomainError" begin
+				for x in [
+					DataFrame(),
+					DataFrame(x = [0,1,2,3]),
+				]
+					@test_throws DomainError pivot(x)
+				end
 			end
 		end
+		@testset verbose = true "Simple examples" begin
+			A = DataFrame(a=[1,2,1], b=['x','y','y'])
+			B = pivot(A, :a, :b)
+			C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
+			@test B == C
 
-		@testset "DomainError" begin
-			for x in [
-				DataFrame(),
-				DataFrame(x = [0,1,2,3]),
-			]
-				@test_throws DomainError pivot(x)
-			end
+			B = pivot(A, :a)
+			@test B == C
+
+			B = pivot(A)
+			@test B == C
+
+			B = pivot(A, "a", "b")
+			@test B == C
+
 		end
+
 	end
-	@testset verbose = true "Simple examples" begin
+
+	#=
+	@testset verbose = true "pivot!()" begin
+
 		A = DataFrame(a=[1,2,1], b=['x','y','y'])
-		B = pivot(A, :a, :b)
+		pivot!(A, :a, :b)
 		C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
-		@test B == C
-
-		B = pivot(A, :a)
-		@test B == C
-
-		B = pivot(A)
-		@test B == C
-
-		B = pivot(A, "a", "b")
-		@test B == C
+		@test A == C
 
 	end
+	=#
 
-end
+	#=
+	@testset verbose = true "wide_to_long()" begin
 
-#=
-@testset verbose = true "pivot!()" begin
+		@testset verbose = true "Intended exceptions" begin
+			@test true
+		end
 
-	A = DataFrame(a=[1,2,1], b=['x','y','y'])
-	pivot!(A, :a, :b)
-	C = DataFrame(a=[1,2], x=[true,false], y=[true,true])
-	@test A == C
+		@testset verbose = true "Simple examples" begin
+			long = DataFrame(
+			       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
+			       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
+			       )
+			sort!(long)
+			display(long)
 
-end
-=#
-
-#=
-@testset verbose = true "wide_to_long()" begin
-
-	@testset verbose = true "Intended exceptions" begin
-		@test true
+			wide = pivot(long)
+			new_long = wide_to_long(wide)
+			@test new_long == long
+		end
 	end
+	=#
 
-	@testset verbose = true "Simple examples" begin
-		long = DataFrame(
-		       name=["aaa","bbb","aaa","ccc","ccc","aaa","aaa","ccc","eee"],
-		       val=['x',   'w',  'w',  'y',  'z',  'q',  'y',  'a',  'w'],
-		       )
-		sort!(long)
-		display(long)
+	#=
+	A = DataFrame(x = [0,1,5,1,2,5,4,3,8,6,9,9,5,1,1,3], 
+		y = ['a','b','c','a','a','a','c','d', 'a','b','c','a','a','a','c','d'],
+		z = ['1','3','3','2','1','4','4','5', '4','3','3','2','2','4','3','1'],
+	)
 
-		wide = pivot(long)
-		new_long = wide_to_long(wide)
-		@test new_long == long
+	US_coins = DataFrame(
+		name = ["Penny","Nickel","Dime","Quarter"],
+		value = [1, 5, 10, 25] .// 100,
+		mass = [2.500, 5.000, 2.268, 5.67],
+	)
+
+	df=DataFrame(name=["aaa","bbb","ccc","ddd"], x=[1,3,4,3], y=[0,1,1,0], z=[0,1,0,1])
+
+
+
+	=#
+
+	@testset verbose = true "get_data()" begin
+		@testset verbose = true "Open files" begin
+			# @test_skip
+			# @test_broken
+			file = joinpath(pkgdir(PreprocessMD), "test", "example.csv")
+			df =File(file, header = 1) |> DataFrame
+			@test true
+		end
 	end
-end
-=#
-
-#=
-A = DataFrame(x = [0,1,5,1,2,5,4,3,8,6,9,9,5,1,1,3], 
-	y = ['a','b','c','a','a','a','c','d', 'a','b','c','a','a','a','c','d'],
-	z = ['1','3','3','2','1','4','4','5', '4','3','3','2','2','4','3','1'],
-)
-
-US_coins = DataFrame(
-	name = ["Penny","Nickel","Dime","Quarter"],
-	value = [1, 5, 10, 25] .// 100,
-	mass = [2.500, 5.000, 2.268, 5.67],
-)
-
-df=DataFrame(name=["aaa","bbb","ccc","ddd"], x=[1,3,4,3], y=[0,1,1,0], z=[0,1,0,1])
-
-
-
-=#
-
-@testset verbose = true "get_data()" begin
-	@testset verbose = true "Open files" begin
-		# @test_skip
-		# @test_broken
-		file = joinpath(pkgdir(PreprocessMD), "test", "example.csv")
-		df =File(file, header = 1) |> DataFrame
-		@test true
-	end
-end
 
 end
 
