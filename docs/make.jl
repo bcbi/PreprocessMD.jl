b17fa70956d9e65bc6075e5d442913b876c05915
Dilum Aluthge <dilum@aluthge.com>
2022-05-07 04:26:45 -0400

    More fixes for the `docs/make.jl` file (#22)

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,26 @@
-using Documenter, PreprocessMD
+import PreprocessMD
+
+using Documenter: HTML
+using Documenter: makedocs
+using Documenter: deploydocs
 
 makedocs(;
-    authors="Ashlin Harris",
+    modules=[PreprocessMD],
+    authors="Ashlin Harris, Brown Center for Biomedical Informatics, and contributors",
+    repo="https://github.com/bcbi/PreprocessMD.jl/blob/{commit}{path}#L{line}",
     sitename="PreprocessMD.jl",
-    pages=["home" => "index.md"],
+    format=HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://bcbi.github.io/PreprocessMD.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
     strict=true,
 )
 
-#https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.deploydocs
 deploydocs(;
-    repo="github.com/bcbi/PreprocessMD.jl.git",
-    versions=["stable" => "v^", "latest" => "v#.#"],
+    repo="github.com/bcbi/PreprocessMD.jl",
+    devbranch="main",
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,12 +1,16 @@
-# PreprocessMD.jl Documentation
+```@meta
+CurrentModule = PreprocessMD
+```
+
+# PreprocessMD
+
+Documentation for [PreprocessMD](https://github.com/bcbi/PreprocessMD.jl).
 
 ```@contents
 ```
 
-
-
-
 ```@docs
+PreprocessMD
 add_label_column!
 pivot
 ```
