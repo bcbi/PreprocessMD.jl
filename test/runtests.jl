f972c6dca7274e37aecf47c2f88039746d4714c7
AshlinHarris <ashlin_harris@brown.edu>
2022-05-09 21:02:33 -0400

    Clean up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,230 +1,79 @@
 using PreprocessMD: PreprocessMD
 
 using Aqua: Aqua
-using DataFrames: DataFrames
-using Test: Test
-using Downloads: Downloads
 using CSV: CSV
-
-using PreprocessMD: add_label_column!
-using PreprocessMD: pivot
-using PreprocessMD: repr
-using PreprocessMD: top_n_values
+using DataFrames: DataFrames
+using Downloads: Downloads
+using Tables: Tables
+using Test: Test
 
 using DataFrames: DataFrame
 using DataFrames: innerjoin
 using DataFrames: summary
-using Tables
+using PreprocessMD: add_label_column!
+using PreprocessMD: pivot
+using PreprocessMD: repr
+using PreprocessMD: top_n_values
 using Test: @testset
 using Test: @test
 using Test: @test_throws
 using Test: @test_skip
 
-@testset "PreprocessMD" begin
-
-	@testset "assert_is_table()" begin
-		@test_throws ArgumentError add_label_column!(12, 12)
-	end
-
+@testset "PreprocessMD" verbose = false begin
 	# All external file downloads
-        PERSON = DataFrame(
-            CSV.File.(
-                Downloads.download(
-                    "https://physionet.org/files/mimic-iv-demo-omop/0.9/1_omop_data_csv/person.csv",
-                )
-            ),
-        )
-        DRUG = DataFrame(
-            CSV.File.(
-                Downloads.download(
-                    "https://physionet.org/files/mimic-iv-demo-omop/0.9/1_omop_data_csv/drug_exposure.csv",
-                )
-            ),
-        )
-        CONDITION = DataFrame(
-            CSV.File.(
-                Downloads.download(
-                    "https://physionet.org/files/mimic-iv-demo-omop/0.9/1_omop_data_csv/condition_occurrence.csv",
-                )
-            ),
-        )
 
-    @testset "File IO" begin
+	url = "https://physionet.org/files/mimic-iv-demo-omop/0.9/1_omop_data_csv"
+	PERSON = DataFrame(CSV.File.(Downloads.download("$url/person.csv")))
+	DRUG = DataFrame(CSV.File.(Downloads.download("$url/drug_exposure.csv")))
+	CONDITION = DataFrame(CSV.File.(Downloads.download("$url/condition_occurrence.csv")))
+	DEATH = DataFrame(CSV.File.(Downloads.download("$url/death.csv")))
 
-        @test summary(PERSON) == "100×18 DataFrame"
-        @test names(PERSON) == [
-            "person_id",
-            "gender_concept_id",
-            "year_of_birth",
-            "month_of_birth",
-            "day_of_birth",
-            "birth_datetime",
-            "race_concept_id",
-            "ethnicity_concept_id",
-            "location_id",
-            "provider_id",
-            "care_site_id",
-            "person_source_value",
-            "gender_source_value",
-            "gender_source_concept_id",
-            "race_source_value",
-            "race_source_concept_id",
-            "ethnicity_source_value",
-            "ethnicity_source_concept_id",
-        ]
+#=
+	@testset "File IO" verbose = false begin
+		@test summary(PERSON) == "100×18 DataFrame"
+		@test names(PERSON) == [
+			"person_id",
+			"gender_concept_id",
+			"year_of_birth",
+			"month_of_birth",
+			"day_of_birth",
+			"birth_datetime",
+			"race_concept_id",
+			"ethnicity_concept_id",
+			"location_id",
+			"provider_id",
+			"care_site_id",
+			"person_source_value",
+			"gender_source_value",
+			"gender_source_concept_id",
+			"race_source_value",
+			"race_source_concept_id",
+			"ethnicity_source_value",
+			"ethnicity_source_concept_id",
+		]
 
-        @test summary(DRUG) == "18229×23 DataFrame"
-        @test summary(CONDITION) == "16441×16 DataFrame"
-    end
-
-    @testset "add_label_column!()" begin
-        @testset "Intended exceptions" begin
-            @testset "DomainError" begin
-                @test_throws DomainError add_label_column!(DataFrame(), DataFrame())
-                @test_throws DomainError add_label_column!(DataFrame(), DataFrame(; x=[]))
-                @test_throws DomainError add_label_column!(DataFrame(; x=[]), DataFrame())
-                @test_throws DomainError add_label_column!(
-                    DataFrame(; x=[]), DataFrame(; x=[])
-                )
-                @test_throws DomainError add_label_column!(
-                    DataFrame(; x=[1, 2]), DataFrame(; x=[])
-                )
-                @test_throws DomainError add_label_column!(
-                    DataFrame(; x=[]), DataFrame(; x=[1, 2])
-                )
-            end
-            @testset "ArgumentError" begin
-
-                # DataFrame definitions
-                long = DataFrame(
-                    name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
-                    val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
-                )
-                short = pivot(long)
-                X = DataFrame(; name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
-                results = DataFrame(;
-                    name=["aaa", "bbb", "ccc", "eee"],
-                    x=[true, false, false, false],
-                    w=[true, true, false, true],
-                    y=[true, false, true, false],
-                    z=[false, false, true, false],
-                    q=[true, false, false, false],
-                    a=[false, false, true, false],
-                    LABEL=[false, true, true, false],
-                )
-
-                #=
-                				new = deepcopy(short)
-                				@test_throws ArgumentError add_label_column!(
-                					new, X, :name, :name, :w
-                				)
-
-                				new = deepcopy(short)
-                				@test_throws ArgumentError add_label_column!(
-                					new, X, :name, :NONEXISTENT
-                				)
-                =#
-
-                new = deepcopy(short)
-                # @test_throws UndefVarError add_label_column!(new, X, :NONEXISTENT)
-            end
-            @testset "MethodError" begin
-                y = DataFrame(x=[1, 2, 3], y=['a', 'b', 'c'])
-                for x in [12, 1.0, "", x -> x]
-                    # @test_throws MethodError add_label_column!(x, x)
-                    # @test_throws MethodError add_label_column!(x, y)
-                    # @test_throws MethodError add_label_column!(y, x)
-                end
-            end
-        end
-	@testset "Table to DataFrame conversions" begin
-		mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
-		mattbl = Tables.table(mat)
-
-            X = DataFrame(name=["bbb", "ccc"], r=["BBB", "CCC"], Column1=[1, 2])
-	
-            add_label_column!(mattbl, X, :Column1)
-		#Tables.getcolumn(mattbl, :Column3) |> display
-		@test true
-		
+		@test summary(DRUG) == "18229×23 DataFrame"
+		@test summary(CONDITION) == "16441×16 DataFrame"
 	end
-        @testset "Default options" begin
+=#
 
-            # DataFrame definitions
-            long = DataFrame(
-                name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
-                val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
-            )
-            short = pivot(long)
-            X = DataFrame(name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
-            results = DataFrame(
-                name=["aaa", "bbb", "ccc", "eee"],
-                x=[true, false, false, false],
-                w=[true, true, false, true],
-                y=[true, false, true, false],
-                z=[false, false, true, false],
-                q=[true, false, false, false],
-                a=[false, false, true, false],
-                LABEL=[false, true, true, false],
-            )
-            new = deepcopy(short)
+	@testset "pivot()" verbose = false begin
+		@testset "Intended exceptions" verbose = false begin
 
-            add_label_column!(new, X, :name)
-            @test new == results
-
-            new = deepcopy(short)
-            add_label_column!(new, X)
-            @test new == results
-        end
-
-        @testset "Simple examples" begin
-
-            # DataFrame definitions
-            long = DataFrame(;
-                name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
-                val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
-            )
-            short = pivot(long)
-            X = DataFrame(; name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
-            results = DataFrame(;
-                name=["aaa", "bbb", "ccc", "eee"],
-                x=[true, false, false, false],
-                w=[true, true, false, true],
-                y=[true, false, true, false],
-                z=[false, false, true, false],
-                q=[true, false, false, false],
-                a=[false, false, true, false],
-                LABEL=[false, true, true, false],
-            )
-            new = deepcopy(short)
-
-            new = deepcopy(short)
-            add_label_column!(new, X, :name, :LABEL)
-            @test new == results
-        end
-    end
-
-	@testset "assert_is_table()" begin
-		@test_throws UndefVarError assert_is_table(12)
-		@test_throws UndefVarError assert_is_table(DataFrame())
-		#TODO: add other test cases (ArgumentError, success)
-	end
-
-    @testset "pivot()" begin
-        @testset "Intended exceptions" begin
-            @testset "MethodError" begin
-                for x in [12, 1.0, "", x -> x]
-                    #@test_throws MethodError pivot(x)
-                end
-            end
-
-            @testset "DomainError" begin
-                for x in [DataFrame(), DataFrame(; x=[0, 1, 2, 3])]
-                    @test_throws DomainError pivot(x)
-                end
-            end
-        end
-	@testset "Table to DataFrame conversions" begin
+			@testset "ArgumentError" verbose = false begin
+			end
+			@testset "DomainError" verbose = false begin
+				for x in [DataFrame(), DataFrame(; x=[0, 1, 2, 3])]
+					@test_throws DomainError pivot(x)
+				end
+			end
+			@testset "MethodError" verbose = false begin
+				for x in [12, 1.0, "", x -> x]
+					#@test_throws MethodError pivot(x)
+				end
+			end
+		end
+	@testset "Table to DataFrame conversions" verbose = false begin
 		mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
 		mattbl = Tables.table(mat)
 
@@ -233,56 +82,196 @@ using Test: @test_skip
 		@test true
 		
 	end
-        @testset "Simple examples" begin
-            A = DataFrame(; a=[1, 2, 1], b=['x', 'y', 'y'])
-            B = pivot(A, :a, :b)
-            C = DataFrame(; a=[1, 2], x=[true, false], y=[true, true])
-            @test B == C
+		@testset "Simple examples" verbose = false begin
+			A = DataFrame(; a=[1, 2, 1], b=['x', 'y', 'y'])
+			B = pivot(A, :a, :b)
+			C = DataFrame(; a=[1, 2], x=[true, false], y=[true, true])
+			@test B == C
 
-            B = pivot(A, :a)
-            @test B == C
+			B = pivot(A, :a)
+			@test B == C
 
-            B = pivot(A)
-            @test B == C
+			B = pivot(A)
+			@test B == C
 
-            B = pivot(A, "a", "b")
-            @test B == C
-        end
-    end
+			B = pivot(A, "a", "b")
+			@test B == C
+		end
+	end
 
-    @testset "Full pipeline" begin
-        p_CONDITION = pivot(CONDITION, :person_id, :condition_concept_id)
-        p_DRUG = pivot(DRUG, :person_id, :drug_concept_id)
+	@testset "add_label_column!()" verbose = false begin
+		@testset "Intended exceptions" verbose = false begin
+			@testset "ArgumentError" verbose = false begin
 
-        p_AGGREGATE = innerjoin(p_CONDITION, p_DRUG; on=:person_id)
+				# DataFrame definitions
+				long = DataFrame(
+					name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
+					val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
+				)
+				short = pivot(long)
+				X = DataFrame(; name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
+				results = DataFrame(;
+					name=["aaa", "bbb", "ccc", "eee"],
+					x=[true, false, false, false],
+					w=[true, true, false, true],
+					y=[true, false, true, false],
+					z=[false, false, true, false],
+					q=[true, false, false, false],
+					a=[false, false, true, false],
+					LABEL=[false, true, true, false],
+				)
+				new = deepcopy(short)
+				# @test_throws UndefVarError add_label_column!(new, X, :NONEXISTENT)
+			end
+			@testset "DomainError" verbose = false begin
+				@test_throws DomainError add_label_column!(DataFrame(), DataFrame())
+				@test_throws DomainError add_label_column!(DataFrame(), DataFrame(; x=[]))
+				@test_throws DomainError add_label_column!(DataFrame(; x=[]), DataFrame())
+				@test_throws DomainError add_label_column!(
+					DataFrame(; x=[]), DataFrame(; x=[])
+				)
+				@test_throws DomainError add_label_column!(
+					DataFrame(; x=[1, 2]), DataFrame(; x=[])
+				)
+				@test_throws DomainError add_label_column!(
+					DataFrame(; x=[]), DataFrame(; x=[1, 2])
+				)
+			end
+			@testset "MethodError" verbose = false begin
+				y = DataFrame(x=[1, 2, 3], y=['a', 'b', 'c'])
+				for x in [12, 1.0, "", x -> x]
+					# @test_throws MethodError add_label_column!(x, x)
+					# @test_throws MethodError add_label_column!(x, y)
+					# @test_throws MethodError add_label_column!(y, x)
+				end
+			end
+		end
+	@testset "Table to DataFrame conversions" verbose = false begin
+	
+	@testset "NonTable" verbose = false begin
 
-        DEATH = DataFrame(
-            CSV.File.(
-                Downloads.download(
-                    "https://physionet.org/files/mimic-iv-demo-omop/0.9/1_omop_data_csv/death.csv",
-                )
-            ),
-        )
-        add_label_column!(p_AGGREGATE, DEATH, :person_id, :death)
+		@test_throws ArgumentError add_label_column!(12, 12)
+	end
+	@testset "Table" verbose = false begin
 
-        @test size(p_AGGREGATE) == (100, 1878)
+		mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
+		mattbl = Tables.table(mat)
 
-        @testset "top_n_values()" begin
-            @test top_n_values(CONDITION, :condition_concept_id, 6) == DataFrame(
-                AbstractVector[
-                    [4145513, 4064452, 4140598, 4092038, 4138456, 433753],
-                    [6531, 2405, 2302, 502, 390, 181],
-                ],
-                DataFrames.Index(
-                    Dict(:condition_concept_id => 1, :nrow => 2),
-                    [:condition_concept_id, :nrow],
-                ),
-            )
-        end
-    end
+			X = DataFrame(name=["bbb", "ccc"], r=["BBB", "CCC"], Column1=[1, 2])
+	
+			add_label_column!(mattbl, X, :Column1)
+		#Tables.getcolumn(mattbl, :Column3) |> display
+		@test true
+	end
+		
+	end
+		@testset "Default options" verbose = false begin
 
-    @testset "Aqua.jl" begin
-        # Aqua.test_all(PreprocessMD) # TODO: uncomment this line
-        Aqua.test_all(PreprocessMD; ambiguities=false) # TODO: delete this line
-    end
+			# DataFrame definitions
+			long = DataFrame(
+				name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
+				val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
+			)
+			short = pivot(long)
+			X = DataFrame(name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
+			results = DataFrame(
+				name=["aaa", "bbb", "ccc", "eee"],
+				x=[true, false, false, false],
+				w=[true, true, false, true],
+				y=[true, false, true, false],
+				z=[false, false, true, false],
+				q=[true, false, false, false],
+				a=[false, false, true, false],
+				LABEL=[false, true, true, false],
+			)
+			new = deepcopy(short)
+
+			add_label_column!(new, X, :name)
+			@test new == results
+
+			new = deepcopy(short)
+			add_label_column!(new, X)
+			@test new == results
+		end
+
+		@testset "Simple examples" verbose = false begin
+
+			# DataFrame definitions
+			long = DataFrame(;
+				name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
+				val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
+			)
+			short = pivot(long)
+			X = DataFrame(; name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
+			results = DataFrame(;
+				name=["aaa", "bbb", "ccc", "eee"],
+				x=[true, false, false, false],
+				w=[true, true, false, true],
+				y=[true, false, true, false],
+				z=[false, false, true, false],
+				q=[true, false, false, false],
+				a=[false, false, true, false],
+				LABEL=[false, true, true, false],
+			)
+			new = deepcopy(short)
+
+			new = deepcopy(short)
+			add_label_column!(new, X, :name, :LABEL)
+			@test new == results
+		end
+	end
+
+	@testset "Full pipeline" verbose = false begin
+		p_CONDITION = pivot(CONDITION, :person_id, :condition_concept_id)
+		p_DRUG = pivot(DRUG, :person_id, :drug_concept_id)
+
+		p_AGGREGATE = innerjoin(p_CONDITION, p_DRUG; on=:person_id)
+
+		add_label_column!(p_AGGREGATE, DEATH, :person_id, :death)
+
+		@test size(p_AGGREGATE) == (100, 1878)
+
+		@testset "top_n_values()" verbose = false begin
+			@testset "ArgumentError" verbose = false begin
+			end
+			@testset "DomainError" verbose = false begin
+			end
+			@testset "MethodError" verbose = false begin
+			end
+			@test top_n_values(CONDITION, :condition_concept_id, 6) == DataFrame(
+				AbstractVector[
+					[4145513, 4064452, 4140598, 4092038, 4138456, 433753],
+					[6531, 2405, 2302, 502, 390, 181],
+				],
+				DataFrames.Index(
+					Dict(:condition_concept_id => 1, :nrow => 2),
+					[:condition_concept_id, :nrow],
+				),
+			)
+		end
+	end
+
+
+	@testset "Aqua.jl" verbose = false begin
+		Aqua.test_all(PreprocessMD; ambiguities=false)
+	end
 end
+
+#=
+@testset "Template" verbose = false begin
+	@testset "generic_function()" verbose = false begin
+		@testset "Intended Exceptions" verbose = false begin
+			@testset "ArgumentError" verbose = false begin
+			end
+			@testset "DomainError" verbose = false begin
+			end
+			@testset "MethodError" verbose = false begin
+			end
+		end
+		@testset "Default options" verbose = false begin
+		end
+		@testset "Simple examples" verbose = false begin
+		end
+	end
+end
+=#
