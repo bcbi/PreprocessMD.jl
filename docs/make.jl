d0bcca4933713974bf24726cb514b38d3e5af740
AshlinHarris <ashlin_harris@brown.edu>
2022-05-12 09:40:46 -0400

    Edit doc/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,17 +5,18 @@ using Documenter: makedocs
 using Documenter: deploydocs
 
 makedocs(;
-    modules=[PreprocessMD],
-    authors="Ashlin Harris, Brown Center for Biomedical Informatics, and contributors",
-    repo="https://github.com/bcbi/PreprocessMD.jl/blob/{commit}{path}#L{line}",
-    sitename="PreprocessMD",
-    format=HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://bcbi.github.io/PreprocessMD.jl",
-        assets=String[],
-    ),
-    pages=["Home" => "index.md"],
-    strict=true,
+	modules=[PreprocessMD],
+	authors="Ashlin Harris, Brown Center for Biomedical Informatics, and contributors",
+	repo="https://github.com/bcbi/PreprocessMD.jl/blob/{commit}{path}#L{line}",
+	sitename="PreprocessMD",
+	format=HTML(;
+		prettyurls=get(ENV, "CI", "false") == "true",
+		canonical="https://bcbi.github.io/PreprocessMD.jl",
+		assets=String[],
+	),
+	pages=["Home" => "index.md"],
+	strict=true,
+	doctest = true
 )
 
 deploydocs(; repo="github.com/bcbi/PreprocessMD.jl", devbranch="main")
