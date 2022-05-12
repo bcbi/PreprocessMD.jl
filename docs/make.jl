5ad6fe83469f058c8746a014f6b440ffc7667898
AshlinHarris <ashlin_harris@brown.edu>
2022-05-12 10:37:31 -0400

    Remove makedocs(format=...)

--- docs/make.jl
+++ docs/make.jl
@@ -12,11 +12,6 @@ makedocs(;
 	authors="Ashlin Harris, Brown Center for Biomedical Informatics, and contributors",
 	repo="https://github.com/bcbi/PreprocessMD.jl/blob/{commit}{path}#L{line}",
 	sitename="PreprocessMD",
-	format=HTML(;
-		prettyurls=get(ENV, "CI", "false") == "true",
-		canonical="https://bcbi.github.io/PreprocessMD.jl",
-		assets=String[],
-	),
 	pages=["Home" => "index.md"],
 	strict=true,
 	doctest = true
