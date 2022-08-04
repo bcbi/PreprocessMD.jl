de6c865d6c39370df92fd349c4645c93b2d6f388
Ashlin Harris <ashlin_harris@brown.edu>
2022-08-04 13:35:43 -0400

    Remove deprecated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,13 +35,6 @@ using Test: @test_skip
 	#CONDITION = DataFrame(File.(download("$url/condition_occurrence.csv")));
 	#DEATH = DataFrame(File.(download("$url/death.csv")));
 
-#=
-	@testset "Medical codes" verbose = false begin
-	x = filter(:ethnicity_concept_id => ==(38003563), PERSON) # Hispanic or Latino
-	# 38003564 # Not Hispanic or Latino
-	@test true
-	end
-=#
 
 
 	@testset "pivot()" verbose = false begin
@@ -67,22 +60,6 @@ using Test: @test_skip
 			end
 		end
 
-#=
-# Removed for 3.0 compatability requirements
-		@testset "Table inputs" verbose = false begin
-			@testset "NonTable" verbose = false begin
-			end
-			@testset "Table" verbose = false begin
-				mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
-				mattbl = table(mat)
-
-				pivot(mattbl)	
-			
-				@test true
-			end
-			
-		end
-=#
 
 		@testset "Simple examples" verbose = false begin
 			A = DataFrame(; a=[1, 2, 1], b=['x', 'y', 'y'])
@@ -150,27 +127,6 @@ using Test: @test_skip
 		end
 
 		@testset "Table inputs" verbose = false begin
-#=
-# Removed for 3.0 compatability requirements
-			@testset "NonTable" verbose = false begin
-
-				@test_throws ArgumentError add_label_column!(12, 12, :NONEXISTENT)
-			end
-=#
-#=
-# Removed for 3.0 compatability requirements
-			@testset "Table" verbose = false begin
-
-				mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
-				mattbl = table(mat)
-
-					X = DataFrame(name=["bbb", "ccc"], r=["BBB", "CCC"], Column1=[1, 2])
-			
-					add_label_column!(X, mattbl, :Column2)
-				#getcolumn(mattbl, :Column3) |> display
-				@test true
-			end
-=#
 		end
 		@testset "Default options" verbose = false begin
 
@@ -268,83 +224,6 @@ using Test: @test_skip
 			end
 		end
 
-#=
-# Removed for 3.0 compatability requirements
-		@testset "Table inputs" verbose = false begin
-			@testset "NonTable" verbose = false begin
-
-				@test_throws ArgumentError set_label_column!(12, :NONEXISTENT)
-			end
-			@testset "Table" verbose = false begin
-
-				mat = [1 4.0 "7"; 2 5.0 "8"; 3 6.0 "9"]
-				mattbl = table(mat)
-
-					X = DataFrame(name=["bbb", "ccc"], r=["BBB", "CCC"], Column1=[1, 2])
-			
-					set_label_column!(mattbl, :Column2)
-				#getcolumn(mattbl, :Column3) |> display
-				@test true
-			end
-		end
-=#
-#=
-		@testset "Default options" verbose = false begin
-
-			# DataFrame definitions
-			long = DataFrame(
-				name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
-				val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
-			)
-			short = pivot(long)
-			X = DataFrame(name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
-			results = DataFrame(
-				name=["aaa", "bbb", "ccc", "eee"],
-				x=[true, false, false, false],
-				w=[true, true, false, true],
-				y=[true, false, true, false],
-				z=[false, false, true, false],
-				q=[true, false, false, false],
-				a=[false, false, true, false],
-				r=[false, true, true, false],
-			)
-			new = deepcopy(short)
-
-			new = deepcopy(short)
-			set_label_column!(new, X, :r)
-			@test new == results
-
-			new = deepcopy(short)
-			set_label_column!(new, X, "r")
-			@test new == results
-		end
-
-		@testset "Simple examples" verbose = false begin
-
-			# DataFrame definitions
-			long = DataFrame(;
-				name=["aaa", "bbb", "aaa", "ccc", "ccc", "aaa", "aaa", "ccc", "eee"],
-				val=['x', 'w', 'w', 'y', 'z', 'q', 'y', 'a', 'w'],
-			)
-			short = pivot(long)
-			X = DataFrame(; name=["bbb", "ccc", "fff"], r=["BBB", "CCC", "FFF"])
-			results = DataFrame(;
-				name=["aaa", "bbb", "ccc", "eee"],
-				x=[true, false, false, false],
-				w=[true, true, false, true],
-				y=[true, false, true, false],
-				z=[false, false, true, false],
-				q=[true, false, false, false],
-				a=[false, false, true, false],
-				val=[false, true, true, false],
-			)
-			new = deepcopy(short)
-
-			new = deepcopy(short)
-			set_label_column!(new, X, :val)
-			@test new == results
-		end
-=#
 
 @testset "Functionality" verbose = false begin
 	X = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover"],
@@ -492,36 +371,6 @@ end
 		end
 	end
 
-	###@testset "Full pipeline" verbose = false begin
-#=
-		p_CONDITION = pivot(CONDITION, :person_id, :condition_concept_id)
-		p_DRUG = pivot(DRUG, :person_id, :drug_concept_id)
-
-		p_AGGREGATE = innerjoin(p_CONDITION, p_DRUG; on=:person_id)
-
-		add_label_column!(p_AGGREGATE, DEATH, :death)
-
-		@test size(p_AGGREGATE) == (100, 1878)
-=#
-
-#=
-
-		medical_codes = DataFrame(
-			CODE = [840544004, 840539006],
-			)
-		#p_medical_codes = pivot(medical_codes)
-		#medical_codes = DataFrame(
-			#"840544004" = [],
-			#"840539006" = [],
-			#)
-		#medical_codes = [840544004, 840539006]
-		println(names(medical_codes))
-		println(names(CONDITION))
-		COVID = subsetMD(CONDITION, medical_codes, :CODE)
-		add_label_column!(p_AGGREGATE, COVID, label)
-=#
-
-	###end
 
 	@testset "top_n_values()" verbose = false begin
 
@@ -530,27 +379,8 @@ df = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover", "Big Bird", "E
 @test top_n_values(df, :fur_color, 4) == DataFrame(AbstractVector[["blue", "yellow", "red", "green"], [3, 2, 1, 1]], Index(Dict(:nrow => 2, :fur_color => 1), [:fur_color, :nrow]))
 
 
-#=
-#Test removed to avoid downloading external files
-		@test top_n_values(CONDITION, :condition_concept_id, 6) == DataFrame(
-			AbstractVector[
-				[4145513, 4064452, 4140598, 4092038, 4138456, 433753],
-				[6531, 2405, 2302, 502, 390, 181],
-			],
-			Index(
-				Dict(:condition_concept_id => 1, :nrow => 2),
-				[:condition_concept_id, :nrow],
-			),
-		)
-=#
 	end
 
-#=
-# Test removed to reduce dependencies
-	@testset "Aqua.jl" verbose = false begin
-		Aqua.test_all(PreprocessMD; ambiguities=false)
-	end
-=#
 
 end
 
