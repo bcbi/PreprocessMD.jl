fcfa66bf332d71f1ec13cf1d6451d07da25a8372
Ashlin Harris <ashlin_harris@brown.edu>
2022-08-12 13:49:28 -0400

    Fill in doctest output for remaining functions

--- src/PreprocessMD.jl
+++ src/PreprocessMD.jl
@@ -114,8 +114,24 @@ Column(s) `y` (all columns besides `x`, by default) become the column names of `
 julia> using DataFrames
 
 julia> df = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover"], fur_color=["blue", "red", "green", "blue"])
+4×2 DataFrame
+ Row │ name            fur_color
+     │ String          String
+─────┼───────────────────────────
+   1 │ Cookie Monster  blue
+   2 │ Elmo            red
+   3 │ Oscar           green
+   4 │ Grover          blue
 
 julia> pivot(df)
+4×4 DataFrame
+ Row │ name            blue   red    green
+     │ String          Bool   Bool   Bool
+─────┼─────────────────────────────────────
+   1 │ Cookie Monster   true  false  false
+   2 │ Elmo            false   true  false
+   3 │ Oscar           false  false   true
+   4 │ Grover           true  false  false
 
 ```
 """
@@ -178,8 +194,26 @@ Designate one column within a DataFrame as the label
 julia> using DataFrames
 
 julia> X = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover"], lovable = [true, true, false, true], furry = [true, true, true, true], old = [false, false, true, true] )
+4×4 DataFrame
+ Row │ name            lovable  furry  old
+     │ String          Bool     Bool   Bool
+─────┼───────────────────────────────────────
+   1 │ Cookie Monster     true   true  false
+   2 │ Elmo               true   true  false
+   3 │ Oscar             false   true   true
+   4 │ Grover             true   true   true
 
 julia> set_label_column!(X,:lovable)
+
+julia> X
+4×4 DataFrame
+ Row │ name            lovable  furry  old
+     │ String          Cat…     Bool   Bool
+─────┼───────────────────────────────────────
+   1 │ Cookie Monster  true      true  false
+   2 │ Elmo            true      true  false
+   3 │ Oscar           false     true   true
+   4 │ Grover          true      true   true
 
 ```
 """
@@ -227,10 +261,31 @@ Filtration step
 julia> using DataFrames
 
 julia> X = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover"], blue = [true, false, false, true], red  = [false, true, false, false], green = [false, false, true, false])
+4×4 DataFrame
+ Row │ name            blue   red    green
+     │ String          Bool   Bool   Bool
+─────┼─────────────────────────────────────
+   1 │ Cookie Monster   true  false  false
+   2 │ Elmo            false   true  false
+   3 │ Oscar           false  false   true
+   4 │ Grover           true  false  false
 
 julia> Y = DataFrame(name=["Big Bird", "Cookie Monster", "Elmo"], fuzzy=[false, true, true])
+3×2 DataFrame
+ Row │ name            fuzzy
+     │ String          Bool
+─────┼───────────────────────
+   1 │ Big Bird        false
+   2 │ Cookie Monster   true
+   3 │ Elmo             true
 
 julia> subsetMD(X,Y)
+2×4 DataFrame
+ Row │ name            blue   red    green
+     │ String          Bool   Bool   Bool
+─────┼─────────────────────────────────────
+   1 │ Cookie Monster   true  false  false
+   2 │ Elmo            false   true  false
 
 ```
 """
@@ -262,8 +317,28 @@ Useful for initial feasibility checks, but medical codes are not considered
 julia> using DataFrames
 
 julia> df = DataFrame(name=["Cookie Monster", "Elmo", "Oscar", "Grover", "Big Bird", "Ernie", "Bert", "Rosita"], fur_color=["blue", "red", "green", "blue", "yellow", "orange", "yellow", "blue"])
+8×2 DataFrame
+ Row │ name            fur_color
+     │ String          String
+─────┼───────────────────────────
+   1 │ Cookie Monster  blue
+   2 │ Elmo            red
+   3 │ Oscar           green
+   4 │ Grover          blue
+   5 │ Big Bird        yellow
+   6 │ Ernie           orange
+   7 │ Bert            yellow
+   8 │ Rosita          blue
 
 julia> top_n_values(df, :fur_color, 4)
+4×2 DataFrame
+ Row │ fur_color  nrow
+     │ String     Int64
+─────┼──────────────────
+   1 │ blue           3
+   2 │ yellow         2
+   3 │ red            1
+   4 │ green          1
 
 ```
 """
