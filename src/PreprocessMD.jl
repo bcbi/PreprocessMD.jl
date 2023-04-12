532b6f1301cf2dae5920b90640e9f5194c9e996b
AshlinHarris <ashlin_harris@brown.edu>
2023-04-12 17:23:18 -0400

    Add doctest

--- src/PreprocessMD.jl
+++ src/PreprocessMD.jl
@@ -5,6 +5,7 @@ Medically-informed data preprocessing for machine learning
 """
 module PreprocessMD
 
+using Dates: Year
 using DataFrames: AbstractDataFrame
 using DataFrames: combine
 using DataFrames: DataFrame
@@ -110,8 +111,38 @@ function add_label_column!(
 	return nothing
 end
 
-function subset_invalid_date(df, col, min, max)
-        filter( x-> !(Year(min) < Year(x[col]) < Year(max) ), df)
+"""
+	function subset_invalid_year
+# Arguments
+#
+# Examples
+```jldoctest
+julia> using PreprocessMD, Dates, DataFrames
+
+julia> X = DataFrame(name=["Kermit", "Big Bird", "Herry", "Mr. Snuffleupagus", "Rosita", "Julia"], first_appearance=Date.(["1955-05-09", "1969-11-10", "1970-11-09", "1971-11-15", "1991-11-26", "2017-04-10"]))
+6×2 DataFrame
+ Row │ name               first_appearance
+     │ String             Dates.Date
+─────┼─────────────────────────────────────
+   1 │ Kermit             1955-05-09
+   2 │ Big Bird           1969-11-10
+   3 │ Herry              1970-11-09
+   4 │ Mr. Snuffleupagus  1971-11-15
+   5 │ Rosita             1991-11-26
+   6 │ Julia              2017-04-10
+
+julia> subset_invalid_year(X, :first_appearance, 1969, 2000)
+2×2 DataFrame
+ Row │ name    first_appearance
+     │ String  Dates.Date
+─────┼──────────────────────────
+   1 │ Kermit  1955-05-09
+   2 │ Julia   2017-04-10
+
+```
+"""
+function subset_invalid_year(df, col, min, max)
+        filter( x-> !(Year(min) <= Year(x[col]) <= Year(max) ), df)
 end
 
 """
